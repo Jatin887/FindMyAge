c765e01ede247e2c62ccc24a51bba9403b349df3
Jatin Fulwani <jatin.fulwani2020@vit.ac.in>
2021-03-22 00:06:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/findmyage/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.findmyage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.annotation.IntegerRes
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        BuGetAge.setOnClickListener {
+            buttonGetAgeClick()
+
+
+
+
+        }
+    }
+    fun buttonGetAgeClick(){
+        // add your code here after clicking buuton
+        val userdob=Integer.parseInt(edDOB.text.toString())
+        val currentyear= Calendar.getInstance().get(Calendar.YEAR)
+        val userageinyears= currentyear-userdob
+        tvShowAge.text="Your age is $userageinyears Years"
+        Log.d("Logsme", "Your age is $userageinyears Years")
+    }
+
+    fun buAgeclick(view: View) {}
+
+}
+
+
